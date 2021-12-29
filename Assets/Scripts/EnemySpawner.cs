82eeb470d4c4a1d72a0c1c8af694073d1ae78b0a
orsmo <smolarskior@gmail.com>
2021-12-29 12:41:36 +0200

    added looping on waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] WaveConfigSO currentWave;
+    [SerializeField] List<WaveConfigSO> waveConfigs;
+    [SerializeField] float timeBetweenWaves = 0f;
+    WaveConfigSO currentWave;
+    [SerializeField] bool isLooping;
 
     void Start()
     {
-        SpawnEnemies();
+        StartCoroutine(SpawnEnemyWaves());
     }
 
-    void SpawnEnemies(){
-        Instantiate(currentWave.GetEnemyPrefab(0),
-        currentWave.GetStartingWaypoint().position,
-        Quaternion.identity);
+    IEnumerator SpawnEnemyWaves(){
+        do {
+            foreach (WaveConfigSO wave in waveConfigs)
+            {
+                currentWave = wave;
+                for( int i = 0; i < currentWave.GetEnemyCount(); i++){
+                    Instantiate(currentWave.GetEnemyPrefab(i),
+                        currentWave.GetStartingWaypoint().position,
+                        Quaternion.identity,transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }   
+                yield return new WaitForSeconds(timeBetweenWaves);
+            }
+        }while (isLooping);
+        
     }
 
     public WaveConfigSO GetCurrentWave(){
--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -9,6 +9,9 @@ public class WaveConfigSO : ScriptableObject
     [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float timeBetweenEnemySpawns = 1f;
+    [SerializeField] float spawnTimeVariance = 0f;
+    [SerializeField] float minimumSpawnTime =0.2f;
 
     public Transform GetStartingWaypoint(){
         return pathPrefab.GetChild(0);
@@ -34,5 +37,11 @@ public class WaveConfigSO : ScriptableObject
     
     public float GetMoveSpeed(){
         return moveSpeed;
+    }
+
+    public float GetRandomSpawnTime(){
+        float spawnTime = Random.Range(timeBetweenEnemySpawns - spawnTimeVariance,
+                                        timeBetweenEnemySpawns + spawnTimeVariance);
+        return Mathf.Clamp(spawnTime, minimumSpawnTime, float.MaxValue);
     }
 }
